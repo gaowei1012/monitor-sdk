b04b8b8513b4cfe10786f54665e47372eb1df868
gaowei1012 <gyb2020018@163.com>
2025-04-15 07:12:34 +0800

    update

--- packages/native/errorBoundary/withErrorBoundary.tsx
+++ packages/native/errorBoundary/withErrorBoundary.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: gaowei1012 gyb2020018@163.com
+ * @Date: 2025-04-08 17:20:22
+ * @LastEditors: gaowei1012 gyb2020018@163.com
+ * @LastEditTime: 2025-04-15 07:12:24
+ * @FilePath: /monitor-sdk/packages/native/errorBoundary/withErrorBoundary.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React, { ComponentType } from 'react'
 import { ErrorBoundary, ErrorBoundaryProps } from './ErrorBoundary'
 
@@ -7,6 +15,7 @@ import { ErrorBoundary, ErrorBoundaryProps } from './ErrorBoundary'
  * @param params.onDidCatch
  * @returns
  */
+
 //  * @example
 //  * ```tsx
 //  * const ErrorBoundaryWrapper = withErrorBoundary({
